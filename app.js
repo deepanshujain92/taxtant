40d47d3b687e2b0c1d101838d7439af1b8fc79d1
deepanshujain92 <deepanshu.gv@gmail.com>
2017-06-25 02:20:17 +0530

    Second Commit

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var config = require('./config');
 
 var app = express();
 
@@ -18,7 +19,7 @@ app.set('view engine', 'jade');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -31,26 +32,99 @@ app.post('/webhook', function (req, res) {
     var body = req.body;
     var action = (body.result.action);
     var params = body.result.parameters;
-    console.log(body);   
+    console.log(body);
     console.log(body.result.resolvedQuery);
-    console.log(lngDetector.detect(body.result.resolvedQuery));
+    var lang = lngDetector.detect(body.result.resolvedQuery, 1)[0][0];
+    console.log(lang);
+    console.log(body);
+    if (action == "gst.defination")
+    {
+        if (lang == "hindi")
+        {
+            var speech;
+            // if(params.AccountType==="Savings") 
+            speech = "माल और सेवाओं की खपत पर यह एक गंतव्य आधारित कर है। यह सभी चरणों में लगाए जाने का प्रस्ताव है, जो कि आखिरी खपत के निर्माण के साथ ही पिछले चरणों में उपलब्ध कराए गए करों के क्रेडिट के साथ सेटऑफ़ के रूप में उपलब्ध हैं। संक्षेप में, केवल मूल्य अतिरिक्त कर लगाया जाएगा और टैक्स का बोझ अंतिम उपभोक्ता द्वारा वहन किया जाना है।";
+//        else speech = "Here is Approved Sanctioned Amount for your "+params.AccountType+" Account" + accountDetails.ApprovedSanctionedAmount;
+
+            var json = {
+                "speech": speech,
+                "displayText": "",
+                "source": "bot"
+            };
+            res.set("Content-type", "application/json")
+            res.send(json);
+        } else
+        {
+            var speech;
+            // if(params.AccountType==="Savings") 
+            speech = "It is a destination based tax on consumption of goods and services. It is proposed to be levied at all stages right from manufacture up to final consumption with credit of taxes paid at previous stages available as setoff. In a nutshell, only value addition will be taxed and burden of tax is to be borne by the final consumer.";
+//        else speech = "Here is Approved Sanctioned Amount for your "+params.AccountType+" Account" + accountDetails.ApprovedSanctionedAmount;
+
+            var json = {
+                "speech": speech,
+                "displayText": "",
+                "source": "bot"
+            };
+            res.set("Content-type", "application/json")
+            res.send(json);
+        }
+    } else if (action === "gst.objectdata")
+    {   var item = {};
+        for(var i=0;i<config.Rates.length;i++)
+        {
+            if(config.Rates[i].item["Item"]==params.objectname)
+            {
+                item = config.Rates[i];
+                break;
+            }
+        }
+        if (lang == "hindi")
+        {
+            var speech;
+            // if(params.AccountType==="Savings") 
+            speech = "GST के पहले: "+item.Existing+"% ,GST के बाद: "+item.Rate+"%";
+//        else speech = "Here is Approved Sanctioned Amount for your "+params.AccountType+" Account" + accountDetails.ApprovedSanctionedAmount;
+
+            var json = {
+                "speech": speech,
+                "displayText": "",
+                "source": "bot"
+            };
+            res.set("Content-type", "application/json")
+            res.send(json);
+        } else
+        {
+            var speech;
+            // if(params.AccountType==="Savings") 
+            speech = "Before GST: "+item.Existing+"% ,After: "+item.Rate+"%";
+//        else speech = "Here is Approved Sanctioned Amount for your "+params.AccountType+" Account" + accountDetails.ApprovedSanctionedAmount;
+
+            var json = {
+                "speech": speech,
+                "displayText":"",
+                "source": "bot"
+            };
+            res.set("Content-type", "application/json")
+            res.send(json);
+        }
+    }
 });
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
